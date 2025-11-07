49bf21b67bf418ced9d10ff451f5473866a17e41
Eduardo Porto Gonçalves <eduardopgmg@gmail.com>
2025-11-07 08:53:34 -0300

    fix: ajusta rotas para login, register e home

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
+// src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// páginas
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Top10 from "./pages/Top10";
 import Library from "./pages/Library";
 import LikedSongs from "./pages/LikedSongs";
 import Search from "./pages/Search";
 import CreatePlaylist from "./pages/CreatePlaylist";
-import Login from "./pages/Login";
 import AdminPanel from "./pages/AdminPanel";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* Login padrão */}
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Cadastro */}
+        <Route path="/register" element={<Register />} />
+
+        {/* Área logada */}
+        <Route path="/home" element={<Home />} />
         <Route path="/top10" element={<Top10 />} />
         <Route path="/library" element={<Library />} />
         <Route path="/likedsongs" element={<LikedSongs />} />
         <Route path="/search" element={<Search />} />
         <Route path="/createplaylist" element={<CreatePlaylist />} />
-        <Route path="/login" element={<Login />} />
+
+        {/* Painel admin */}
         <Route path="/admin" element={<AdminPanel />} />
+
+        {/* Qualquer rota desconhecida volta pro login */}
+        <Route path="*" element={<Login />} />
       </Routes>
     </Router>
   );
